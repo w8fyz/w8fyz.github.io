8415191e7350e14bf4d07dc1f47636c3bca1e018
Fyz <35635613+w8fyz@users.noreply.github.com>
2024-04-04 03:30:28 +0200

    Start adding projects, almost finished fronts

--- js/projects.js
+++ js/projects.js
@@ -35,13 +35,10 @@ class Project {
 }
 
 const projects = [
-    new Project("Salut", "test.html",  ["Site Web", "Caca"], "Moi j'adore les super projets rempli de projets, surtout quand le projet s'appelle projet ! Et du coup voila le projet tu vois l'idée ;)"),
-    new Project("Salut", "test.html",  ["Site Web", "Caca"], "Moi j'adore les super projets rempli de projets, surtout quand le projet s'appelle projet ! Et du coup voila le projet tu vois l'idée ;)"),
-    new Project("Salut", "test.html",  ["Site Web", "Caca"], "Moi j'adore les super projets rempli de projets, surtout quand le projet s'appelle projet ! Et du coup voila le projet tu vois l'idée ;)"),
-    new Project("Salut", "test.html",  ["Site Web", "Caca"], "Moi j'adore les super projets rempli de projets, surtout quand le projet s'appelle projet ! Et du coup voila le projet tu vois l'idée ;)"),
-    new Project("Salut", "test.html",  ["Site Web", "Caca"], "Moi j'adore les super projets rempli de projets, surtout quand le projet s'appelle projet ! Et du coup voila le projet tu vois l'idée ;)"),
-    new Project("Salut", "test.html",  ["Site Web", "Caca"], "Moi j'adore les super projets rempli de projets, surtout quand le projet s'appelle projet ! Et du coup voila le projet tu vois l'idée ;)"),
-
+    new Project("Mon portfolio", "portfolio.html",  ["Site Web", "HTML", "CSS", "JS"], "Un portfolio réalisé dans le cadre de ma formation en BTS SIO SLAM, permettant de centraliser mes projets et mes recherches."),
+    new Project("Beepot", "beepot.html", ["Site Web", "HTML", "CSS", "JS", "PHP"], "Un réseau social développé à l'origine comme projet de fin de première année de BTS SIO SLAM, qui a fini par devenir un de mes projets principaux pendant quelques mois.", "beepot.jpg"),
+    new Project("Blog en PHP", "php_blog.html", ["Site Web", "HTML", "CSS", "PHP"], "Lors de ma première année de cours de BTS, nous avons été confronté à un projet aléatoire avec 3h pour le réalisé, c'est à ce moment là que j'ai créer ce système de blog simpliste."),
+    new Project("Wrapper  Java de l'API Ship24", "wrapper_ship4j.html", ["Wrapper", "Java", "Postman"], "J'ai pendant quelques jours travaillé sur la création d'un wrapper pour l'API de Ship24. L'objectif était de pouvoir utiliser facilement leur API dans un programme Java.")
 ]
 
 
