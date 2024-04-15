fe858b74a9421e5998c470abb20ecfdfa2b83ee2
Fyz <35635613+w8fyz@users.noreply.github.com>
2024-04-15 16:10:39 +0200

    update veille

--- js/veille.js
+++ js/veille.js
@@ -19,7 +19,16 @@ class Veille {
 }
 
 let veilles = [
-    new Veille("API Bukkit très stabilisée tavu le truc", "https://google.com", "03/04/2023", "Thibeau Benet"),
+    new Veille("Clone de l'ancien repo de CraftBukkit", "https://hub.spigotmc.org/stash/projects/SPIGOT/repos/craftbukkit/commits?until=refs%2Fheads%2Fversion%2F1.8", "21/12/2010 - aujourd'hui", "Dinnerbone"),
+    new Veille("Repo et documentation des projets liés à Spigot", "https://hub.spigotmc.org/stash/projects/SPIGOT/", "09/09/2022 - aujourd'hui", "md5"),
+    new Veille("10 ans de SpigotMC", "https://www.spigotmc.org/threads/celebrating-10-years-of-spigotmc.579779/", "21/11/2022", "md5"),
+    new Veille("Spigot & BungeeCord 1.19.3", "https://www.spigotmc.org/threads/spigot-bungeecord-1-19-3.581396/", "07/12/2022", "md5"),
+    new Veille("Téléchargement de Spigot", "https://getbukkit.org/download/spigot", "05/12/2023", "GetBukkit Staff"),
+    new Veille("Hub Jira (Bug report) de SpigotMC", "https://hub.spigotmc.org/jira/secure/Dashboard.jspa", "28/11/2014 - aujourd'hui", "md5"),
+    new Veille("Faux Positifs Windows Defender", "https://www.spigotmc.org/threads/windows-defender-false-positives.639507/", "10/03/2024", "md5"),
+    new Veille("Apparition de la 1.20.5", "https://www.minecraft.net/en-us/article/minecraft-1-20-5-pre-release-1", "10/04/2024", "Java Team"),
+    new Veille("Hub Nexus de SpigotMC", "https://hub.spigotmc.org/nexus/#browse", "27/02/2015 - aujourd'hui", "md5"),
+    new Veille("wiki.vg - Wiki de l'API Mojang", "https://wiki.vg/Mojang_API", "17/06/2014 - aujourd'hui", "Thinkofdeath"),
 ]
 
 
