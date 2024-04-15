1fb317d3dc1800fedf439eeeff2dd461b53c8e10
Fyz <35635613+w8fyz@users.noreply.github.com>
2024-04-15 14:51:00 +0200

    add projects

--- js/projects.js
+++ js/projects.js
@@ -42,17 +42,29 @@ const projects = [
     new Project("Mon portfolio", "portfolio.html",  ["Site Web", "HTML", "CSS", "JS"], "Un portfolio réalisé dans le cadre de ma formation en BTS SIO SLAM, permettant de centraliser mes projets et mes recherches."),
     new Project("Beepot", "beepot.html", ["Site Web", "HTML", "CSS", "JS", "PHP"], "Un réseau social développé à l'origine comme projet de fin de première année de BTS SIO SLAM, qui a fini par devenir un de mes projets principaux pendant quelques mois.", "beepot.jpg"),
     new Project("Blog en PHP", "php_blog.html", ["Site Web", "HTML", "CSS", "PHP"], "Lors de ma première année de cours de BTS, nous avons été confronté à un projet aléatoire avec 3h pour le réalisé, c'est à ce moment là que j'ai créer ce système de blog simpliste."),
-    new Project("SDK Java de l'API Ship24", "wrapper_ship4j.html", ["SDK", "Java", "Postman"], "J'ai pendant quelques jours travaillé sur la création d'un wrapper pour l'API de Ship24. L'objectif était de pouvoir utiliser facilement leur API dans un programme Java.")
+    new Project("SDK Java de l'API Ship24", "wrapper_ship4j.html", ["SDK", "Java", "Postman"], "J'ai pendant quelques jours travaillé sur la création d'un wrapper pour l'API de Ship24. L'objectif était de pouvoir utiliser facilement leur API dans un programme Java."),
+    new Project("Bot Discord pour BTS", "bot_bts.html", ["Bot", "Java", "Discord"], "Pour permettre à ma classe un lieu pour échanger, j'ai développé un bot ainsi qu'un serveur discord permettant d'échanger différentes informations.", "bts_bot.jpg"),
+    new Project("Portfolio utilisant Bootstrap et Handlebars", "portfolio_bootstrap_handlebars.html", ["Site Web", "HTML", "Bootstrap", "Handlebars"], "En cours de développement lors de ma première année, il nous a été demandé de réaliser un portfolio avec Bootstrap."),
+    new Project("Logiciel de gestion de couleurs DMX", "gestion_dmx.html", ["Java", "JavaFX"], "En stage dans un studio, nous avons mis en place un système de lumières en DMX, j'ai alors créé un logiciel permettant de choisir la couleur."),
+    new Project("Site Web de gestion d'images", "gestion_images.html", ["Site Web", "HTML", "CSS", "PHP"], "Projet abandonné car jamais utilisé, il permettait de lier des images à des comptes de productions permettant une organisation optimum."),
+    new Project("OkCongés", "okconges.html", ["Site Web", "HTML", "CSS", "PHP"], "Un projet encore utilisé aujourd'hui permettant de notifier des absences et congés et de les faire valider directement en ligne.", "okconges.jpg"),
+    new Project("Site Web d'une filiale de OpenMediaFactory", "filiale_omf.html", ["Site Web","Wordpress"], "Un site web réalisé sur wordpress pour une filiale journalistique de OpenMediaFactory."),
+    new Project("Site Web de TéléParis", "teleparis.html", ["Site Web","Wordpress"], "Le site web de mon entreprise en alternance. Un projet de site Wordpress avec un thème customisé adapté aux besoins de l'entreprise.", "teleparis.jpg"),
+    new Project("Site Web de OpenMediaFactory", "openmediafactory.html", ["Site Web","Wordpress"], "Le site web de la maison-mère de l'entreprise de mon alternance, qui devais mettre en avant les différents services de studios.", "openmediafactory.jpg"),
+
 ]
 
 function getProjectByPath(path) {
-
+    projects.forEach((project) => {
+        if(project.path === path) {
+            return project;
+        }
+    })
+    return null;
 }
 
 function updateSelfFromProject(project) {
     document.title = project.name;
-    document.getElementById("meta-project-tags").innerHTML = getCleanTags(project.tags)
-
 }
 
 let container = document.getElementById("projects-container");
